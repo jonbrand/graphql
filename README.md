c196403fad049089f73c948edb04437534b8570e
jonbrand <jonbranaraujo18@gmail.com>
2022-09-05 13:57:32 -0300

    Schema & Query | Array | Non-Null

---
+++ README.md
@@ -0,0 +1,10 @@
+# Documentação
+
+## Scalar Types
+Server para definir as propriedades dos objetos.
+
+- Int
+- Float
+- String
+- Boolean
+- ID
--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const { gql, ApolloServer } = require("apollo-server");
 
+// definicao de tipos dos valores
+const typeDefs = gql`
+  type Query {
+    idade: Int
+    salario: Float
+    nome: String
+    ativo: Boolean
+    id: ID
+    tecnologias: [String!]!
+  }
+`;
+
+// definicao de valores
 const resolvers = {
   Query: {
-    hello() {
-      return 'World'
+    idade() {
+      return 26;
+    },
+    salario() {
+      return 10000,00;
+    },
+    nome() {
+      return "Jonatas";
+    },
+    ativo() {
+      return true;
+    },
+    id() {
+      return 123456789
+    },
+    tecnologias() {
+      return [ "Graphql", "Node", "ReactJS"]
     }
   }
 }
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`
 
 const server = new ApolloServer({
   typeDefs,
