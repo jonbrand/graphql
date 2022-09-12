25a694985cb263ac3fd17b28a188e07482579669
jonbrand <jonbranaraujo18@gmail.com>
2022-09-12 16:50:47 -0300

    refactor: schema and query

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
 const { gql, ApolloServer } = require("apollo-server");
 
-const produtos = [
-  {
-    id: 1,
-    nome: 'Queijo',
-    valor: 10.00,
-  },
-  {
-    id: 2,
-    nome: 'Uva',
-    valor: 15.00,
-  }
-];
-
-const usuarios = [
-  {
-    id: 1,
-    nome: 'Jonatas',
-    idade: 26,
-    ativo: true,
-    salario: 10000.00,
-    tecnologias: [ "Graphql", "Node", "ReactJS"]
-  },
-  {
-    id: 2,
-    nome: 'Isiane',
-    idade: 24,
-    ativo: true,
-    salario: 15000.00,
-    tecnologias: ["ReactJS"]
-  },
-  {
-    id: 3,
-    nome: 'Davi',
-    idade: 26,
-    ativo: true,
-    salario: 20000.00,
-    tecnologias: ["Node", "ReactJS"]
-  },
-]
+const db = {
+  produtos: [
+    {
+      id: 1,
+      nome: 'Queijo',
+      valor: 10.00,
+    },
+    {
+      id: 2,
+      nome: 'Uva',
+      valor: 15.00,
+    }
+  ],
+  usuarios: [
+    {
+      id: 1,
+      nome: 'Jonatas',
+      idade: 26,
+      ativo: true,
+      salario: 10000.00,
+      tecnologias: [ "Graphql", "Node", "ReactJS"],
+      perfil: 2
+    },
+    {
+      id: 2,
+      nome: 'Isiane',
+      idade: 24,
+      ativo: true,
+      salario: 15000.00,
+      tecnologias: ["ReactJS"],
+      perfil: 1
+    },
+    {
+      id: 3,
+      nome: 'Davi',
+      idade: 26,
+      ativo: true,
+      salario: 20000.00,
+      tecnologias: ["Node", "ReactJS"],
+      perfil: 2
+    },
+  ],
+  perfis: [
+    { id: 1, descricao: "ADMIN" },
+    { id: 2, descricao: "NORMAL" },
+  ]
+}
 
 // definicao de tipos dos valores
 const typeDefs = gql`
+  enum TipoPerfil {
+    ADMIN
+    NORMAL
+  }
+
   type Produto {
     id: ID
     nome: String
@@ -55,30 +68,53 @@ const typeDefs = gql`
     ativo: Boolean
     id: ID
     tecnologias: [String!]!
+    perfil: Perfil
   }
   
+  type Perfil {
+    id: Int
+    descricao: TipoPerfil
+  }
+
   type Query {
     usuarios: [Usuario]
     produtos: [Produto]
+    perfis: [Perfil]
     usuario(id: Int, nome: String): Usuario 
+    produto(id: Int, nome: String): Produto 
   }
 `;
 
 // definicao de valores
 const resolvers = {
+  Usuario: {
+    perfil(usuario) {
+      return db.perfis.find(perfil => perfil.id === usuario.perfil)
+    }
+  },
   Query: {
     usuarios() {
-      return usuarios
-    },
-    produtos() {
-      return produtos
+      return db.usuarios
     },
     usuario(_, args) {
       const { id, nome } = args;
 
-      if (id) return usuarios.find((usuario) => usuario.id === id);
+      if (id) return db.usuarios.find((usuario) => usuario.id === id);
 
-      return usuarios.find((usuario) => usuario.nome === nome);
+      return db.usuarios.find((usuario) => usuario.nome === nome);
+    },
+    produtos() {
+      return db.produtos
+    },
+    produto(_, args) {
+      const { id, nome } = args;
+
+      if (id) return db.produtos.find((produto) => produto.id === id);
+
+      return db.produtos.find((produto) => produto.nome === nome);
+    },
+    perfis() {
+      return db.perfis
     }
   }
 }
